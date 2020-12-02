b3c972662a6adac9c8f9226947e24c2d5cbc4001
Oliver Praesto <oliver.praesto@gmail.com>
2020-12-02 22:51:38 +0100

    - solves day2

--- day2/mod.ts
+++ day2/mod.ts
@@ -4,7 +4,7 @@
 
 import puzzle from './puzzle.ts';
 
-const solution1 = () => {
+const part1 = () => {
     let lines = puzzle.split(/\n/);
     let validPasswords = 0;
 
@@ -21,7 +21,34 @@ const solution1 = () => {
         validPasswords += min <= occurances && occurances <= max ? 1 : 0;
     });
 
-    console.log(validPasswords);
+    console.log(`Part 1: ${validPasswords}`);
 };
 
-solution1();
+const part2 = () => {
+    let lines = puzzle.split(/\n/);
+    let validPasswords = 0;
+
+    lines.forEach((line) => {
+        const parts = line.split(':');
+        const instructions = parts[0].split(' ');
+        const password = parts[1].trim();
+        const minMax = instructions[0].split('-');
+        const letter = instructions[1];
+        const pos1 = +minMax[0];
+        const pos2 = +minMax[1];
+        const validPosition1 = password[pos1 - 1] === letter;
+        const validPosition2 = password[pos2 - 1] === letter;
+
+        if (
+            (validPosition1 && !validPosition2) ||
+            (!validPosition1 && validPosition2)
+        ) {
+            validPasswords += 1;
+        }
+    });
+
+    console.log(`Part 2: ${validPasswords}`);
+};
+
+part1();
+part2();
